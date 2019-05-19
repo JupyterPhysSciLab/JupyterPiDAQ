52aca469bcb8d152d58f1c70d2f8a2115660fb86
gutow <gutow@uwosh.edu>
2019-05-19 16:56:47 -0500

    Reformat and fix typos in setup.py. Create initial README.

---
+++ README.md
@@ -0,0 +1,10 @@
+**JupyterPiDAQ**
+
+This software allows collection and plotting of analog data
+collected using an ADS1115 A-to-D piHAT on a Raspberry Pi.
+So far this has been tested on a RPi 3B+.
+
+Author: Jonathan Gutow <jgutow@new.rr.com>
+
+Installation is meant to be done into a virtual environment
+from the PyPi repository. Instructions...
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
-with open("README.md","r") as f:
-    long_description=f.read()
-    
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
-    name = "JupyterPiDAQ",
-    version = "0.5.0",
-    description = "Data Acquisition in Jupyter notebook on Raspberry Pi",
-    long_description = long_description,
-    author = "Jonathan Gutow"
-    author_email= "jgutow@new.rr.com",
-    license = "GPL-3.0+",
+    name="JupyterPiDAQ",
+    version="0.5.0",
+    description="Data Acquisition in Jupyter notebook on Raspberry Pi",
+    long_description=long_description,
+    author="Jonathan Gutow",
+    author_email="jgutow@new.rr.com",
+    license="GPL-3.0+",
     packages=setuptools.find_packages(),
-    install_requires = [
+    install_requires=[
         'python>=3.6',
         'Adafruit-ADS1x15>=1.0.2',
         'numpy>=1.13',
@@ -20,10 +20,11 @@ setuptools.setup(
         'jupyter>=1.0.0',
         'jupyter-contrib-nbextensions>=0.5.1',
         'pandas>=0.22.0'
-    ]
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers :: End Users',
         'License :: GPL-3.0+',
         'Programming Language :: Python 3'
     ]
+)
