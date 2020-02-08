8c1400a642052199e431ac840866ae985af422a7
gutow <gutow@uwosh.edu>
2020-02-08 14:52:18 -0600

    Adding instructor menu with table creation and cell locking

---
+++ javascript/InstructorToolsmnu.js
@@ -0,0 +1,70 @@
+function createInstructorToolsMenu(){
+    if(!document.getElementById('InstructorToolsmnu')){
+        var newselect=document.createElement('select');
+        newselect.id = 'InstructorToolsmnu';
+        newselect.classList.add('form-control'); //class to match notebook formatting
+        newselect.classList.add('select-xs'); //class to match notebook formatting
+        newselect.onchange=function(){
+            var lastvalue = this.value;
+            this.value='Instructor Tools';
+            if (lastvalue=='Insert Data Table...'){
+                get_table_dim();
+            }
+            if (lastvalue=='Protect Selected Cells'){
+                protect_selected_cells();
+            }
+            if (lastvalue=='Deprotect Selected Cells'){
+                deprotect_selected_cells();
+            }
+            if (lastvalue=='Indicate Protected Cells'){
+                mark_protected_cells();
+            }
+            if (lastvalue=='Deactivate this menu'){
+                deleteInstructorToolsMenu();
+            }
+        }
+        var optiontxt = '<option title="Insert an Instructor Tool.">Instructor Tools</option>';
+        optiontxt+='<option title="Insert cell below selected and create a data entry table.">Insert Data Table...</option>';
+        optiontxt+='<option title="Prevent editting of selected cells.">Protect Selected Cells</option>';
+        optiontxt+='<option title="Allow editting of selected cells.">Deprotect Selected Cells</option>';
+        optiontxt+='<option title="Temporarily highlight protected cells in pink.">Indicate Protected Cells</option>';
+        optiontxt+='<option><hr/></option>';
+        optiontxt+='<option title="Remove/deactivate this menu. Use python command `instmenu_act()` to reactivate">'
+        optiontxt+='Deactivate this menu</option>';
+        newselect.innerHTML=optiontxt;
+        document.getElementById('maintoolbar-container').appendChild(newselect);
+    }
+}
+
+function deleteInstructorToolsMenu(){
+    if(document.getElementById('InstructorToolsmnu')){
+        document.getElementById('InstructorToolsmnu').remove();
+    }
+}
+
+function protect_selected_cells(){
+    var celllist = Jupyter.notebook.get_selected_cells();
+    for (var i = 0;i<celllist.length;i++){
+        celllist[i].metadata.editable=false;
+        celllist[i].element.children()[0].setAttribute("style","background-color:pink;");
+        }
+}
+
+function deprotect_selected_cells(){
+    var celllist = Jupyter.notebook.get_selected_cells();
+    for (var i = 0;i<celllist.length;i++){
+        celllist[i].metadata.editable=true;
+        celllist[i].element.children()[0].removeAttribute("style");
+    }
+}
+
+function mark_protected_cells(){
+    var celllist = Jupyter.notebook.get_cells();
+    for (var i = 0;i<celllist.length;i++){
+        if (celllist[i].metadata.editable==false){
+        celllist[i].element.children()[0].setAttribute("style","background-color:pink;");
+        } else {
+        celllist[i].element.children()[0].removeAttribute("style");
+        }
+    }
+}
