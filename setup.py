c74d959af455edc6af3675caa7026416e4eeb8ba
gutow <gutow@uwosh.edu>
2020-07-13 16:32:54 -0500

    bump to 0.6.0rc2 with addition of pH sensors

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="JupyterPiDAQ",
-    version="0.6.0rc1",
+    version="0.6.0rc2",
     description="Data Acquisition in Jupyter notebook on Raspberry Pi",
     long_description=long_description,
     long_description_content_type="text/markdown",
